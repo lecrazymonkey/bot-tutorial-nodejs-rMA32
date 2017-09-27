953fc2542a55ac07e930faae6151bc3ff230c8f1
lecrazymonkey <32345716+lecrazymonkey@users.noreply.github.com>
2017-09-27 19:28:50 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\//;  botSDL = /^\/scd/
+      botRegex = /^\//;  botscd = /^\/scd/
        
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
@@ -18,7 +18,7 @@ function respond() {
     this.res.end();
   }
   
-  else if(request.text && botSDL.test(request.text)) {
+  else if(request.text && botscd.test(request.text)) {
 this.res.writeHead(200);
 postMessage("https://daddyleagues.com/mpb/schedules");
 this.res.end();
