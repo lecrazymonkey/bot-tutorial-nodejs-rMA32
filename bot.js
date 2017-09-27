76624ce917cc93b0be4c18958d63112b5a57abde
lecrazymonkey <32345716+lecrazymonkey@users.noreply.github.com>
2017-09-27 19:27:43 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,14 +5,8 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\//;  botSDL = /^\/sdl/;botRegexSalt = /^\//;botRegexRules = /^\//
-      botRegexAd=/^\/a/;botRegexGTA = /^\//; botRegexSC = /^\//i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\//;
-      botRegexP = /^\//i;  botRegexTw = /^\//i; botRegexSb = /^\//; botRegexSh = /^\//; botRegexWk = /^\//; botRegexCC = /^\//;
-      botRegexSiege = /^\//; botRegexOW = /^\//; 
-      botRegexSlut = /^\//; botRegexMason = /^\//;
-      botRegexProp = /^\//;botRegexKys = /^\//; botRegexSlam = /^\//; botRegexBrye = /^\//;
-      botRegexGian = /^\//; botRegexScrotes = /^\//; botRegexShaki = /^\//;
-      botRegexDaf = /^\//; botRegexMA32 = /^\//; botRegexTrade = /^\//; 
+      botRegex = /^\//;  botSDL = /^\/sdl/
+       
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
