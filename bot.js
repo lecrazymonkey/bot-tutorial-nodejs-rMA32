d1e40a276a60a1fbfc3c294b89db5e628717b66d
lecrazymonkey <32345716+lecrazymonkey@users.noreply.github.com>
2017-09-29 13:10:44 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,14 +18,14 @@ function respond() {
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
                 ,"BAL","SD","DEN","MIN","ATL","KC","NYG","GB","DET","HOU","STL","CHI","CAR",
                 "MIA","BUF","SF","WAS","NYJ","TB"]
-  if(request.text && botRegexSDL.test(request.text)) {
+  if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage(cool());
     this.res.end();
   }
-  else if(request.text && botRegex.test(request.text)) {
+  else if(request.text && botRegexblog.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("https://daddyleagues.com/mpb/team/schedule");
+    postMessage("https://daddyleagues.com/mpb/blog");
     this.res.end();
   } 
   else if(request.text && botRegexProp.test(request.text)) {
